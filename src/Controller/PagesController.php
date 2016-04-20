73c7200007fbaadb6417db47c2a53e7b3f0993d4
Haran Camatti <hbcamatti1@gmail.com>
2016-04-20 03:07:39 -0300

    atualizando controller

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -37,11 +37,29 @@ class PagesController extends AppController
      */
     public function display()
     {
-        $this->paginate = [
-            'contain' => ['Categories', 'Users']
-        ];
-        $this->set('articles', $this->paginate($this->Articles));
-        $this->set('_serialize', ['articles']);
-    }
+        $path = func_get_args();
+
+        $count = count($path);
+        if (!$count) {
+            return $this->redirect('/');
+        }
+        $page = $subpage = null;
+
+        if (!empty($path[0])) {
+            $page = $path[0];
+        }
+        if (!empty($path[1])) {
+            $subpage = $path[1];
+        }
+        $this->set(compact('page', 'subpage'));
+
+        try {
+            $this->render(implode('/', $path));
+        } catch (MissingTemplateException $e) {
+            if (Configure::read('debug')) {
+                throw $e;
+            }
+            throw new NotFoundException();
+        }
     }
 }
